64bc411ebe9d4f63c384d2ca61675307a9c678d1
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-10-25 09:29:53 +0200

    Add files via upload

---
+++ vjezbaString5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaString5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst;
+
+            Console.WriteLine("Upišite jedan cijeli broj i jedan decimalan broj sa razmakom između njih.");
+
+            tekst = Console.ReadLine();
+
+            string[] brojevi = tekst.Split(' ');
+
+            int.TryParse(brojevi[0], out int BrojC);
+
+            double.TryParse(brojevi[1], out double BrojD);
+
+            Console.WriteLine(BrojC*2);   
+
+            Console.WriteLine(BrojD*2);  
+
+            Console.ReadKey();
+
+        }
+    }
+}
